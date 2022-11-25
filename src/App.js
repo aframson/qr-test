f4e2ac5db945031f73b9f78e5b351ababc5d62eb
aframson <obiriaframson@gmail.com>
2022-11-25 18:32:26 +0000

    this is the

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
+import './styles.css'
+import logo from './HASKE.png'
+import { MdRestaurant } from "react-icons/md";
+import { AiOutlineScan, AiOutlineUser } from "react-icons/ai";
 
 const App = (props) => {
-  const [data, setData] = useState('No result');
+  const [data, setData] = useState('No result Yet');
 
   return (
     <>
       <QrReader
+        containerStyle={{
+          height: 'auto',
+          width: '100%'
+        }}
+        videoContainerStyle={{
+          height: 'auto',
+        }}
+        videoStyle={{
+          height: 'auto',
+          width: '100%'
+        }}
         constraints={{
-          facingMode:"environment"
+          facingMode: "environment"
         }}
         onResult={(result, error) => {
           if (!!result) {
             setData(result?.text);
           }
-
           if (!!error) {
             console.info(error);
           }
         }}
-        style={{ width: '100%' }}
+      // style={{ width: '100%' }}
       />
-      <p>{data}</p>
+      <center>
+        <img src={logo} className={'img'} alt="logo" />
+      </center>
+      <div className='contentbox'>
+        Point the camera at the QR-code and wait for autofocus. The app will scan the code and forward
+        you to the establishment
+        <p>{data}</p>
+      </div>
+      <div className='menu'>
+        <div className='item'>
+          <div className='ibox'>
+            <MdRestaurant size={30} color="black" />
+            <div className='mtxt'>Restaurants</div>
+          </div>
+        </div>
+        <div className='item'>
+          <div className='ibox'>
+            <AiOutlineScan size={30} color="#00D21A" />
+            <div className='mtxt'>Scan</div>
+          </div>
+        </div>
+        <div className='item'>
+          <div className='ibox'>
+            <AiOutlineUser size={30} color="black" />
+            <div className='mtxt'>Register</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
